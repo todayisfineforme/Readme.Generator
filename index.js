b49f08d9c4d2233f03f430b004fbac452debf608
Nathan Hall <Nashing1@gmail.com>
2020-06-06 12:39:33 -0500

    added necessary files from hw folder
    plus changes that i had started to make

---
+++ index.js
@@ -0,0 +1,56 @@
+const inquirer = require('inquirer');
+const fs = require(fs);
+const questions = [{
+    type: input,
+    name: first,
+    message: 'Project Name:'
+},
+{
+    type: input,
+    name: second,
+    message: 'Description:'
+},
+{
+    type: input,
+    name: third,
+    message: 'Table of Contents:'
+},
+{
+    type: input,
+    name: fourth,
+    message: 'Installation:'
+},
+{
+    type: input,
+    name: fifth,
+    message: 'Usage:'
+},
+{
+    type: input,
+    name: sixth,
+    message: 'License:'
+},
+{
+    type: input,
+    name: seventh,
+    message: 'Contributing:'
+},
+{
+    type: input,
+    name: eighth,
+    message: 'Tests:'
+},
+{
+    type: input,
+    name: ninth,
+    message: 'Questions:'
+}];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
